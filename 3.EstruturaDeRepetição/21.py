e37633799179e29beaeadd2b9bed714103c7b8eb
Jônatas Milan <jonatasmilanc@gmail.com>
2024-08-31 10:49:51 -0300

    Exercícios Python Brasil

--- 3.EstruturaDeRepetição/21.py
+++ 3.EstruturaDeRepetição/21.py
@@ -1,18 +1,16 @@
 # Faça um programa que peça um número inteiro e determine se ele é ou não um número primo. 
 # Um número primo é aquele que é divisível somente por ele mesmo e por 1.
 
-def is_prime(num):
-    if num <= 1:
-        return False
-    i = 2
-    while i * i <= num:
-        if num % i == 0:
-            return False
-        i += 1
-    return True
 
-number = int(input('Insira um número: '))
-if is_prime(number):
-    print(f"{number} é primo")
+num = int(input ("Digite um numero inteiro: "))
+div = 0
+
+for i in range(1, num + 1):
+    if num % i == 0:
+        div += 1
+
+if div == 2:
+    print(f'O número {num} é primo.')
 else:
-    print(f"{number} não é primo")
+    print(f'O número {num} não é primo.')
+        
